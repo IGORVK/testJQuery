ac7e808fe29dc834a33f6637f2b3006c4c59bc47
IGORVK <igor.haletskiy@gmail.com>
2016-05-31 11:36:44 +0300

    JQuery -  how this work - look file myscript.js

---
+++ js/testProto.js
@@ -0,0 +1,21 @@
+var animal = {
+name: 'Jack',
+age:2
+
+};
+
+
+function Company() {
+
+
+
+};
+
+    var Constructor = function() {
+        this.a = 1;
+        this.b = 2;
+    }
+
+    var object = Constructor();
+    
+    console.log(animal.prototype);
